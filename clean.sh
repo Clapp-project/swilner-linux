813106930db2ed0464469da9a2e5757c450eeeef
Lamin(Tatsuto Yamamoto) <40442980+laminne@users.noreply.github.com>
2020-07-01 17:54:13 +0900

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -3,7 +3,9 @@
 script_path="$(readlink -f ${0%/*})"
 
 cd "${script_path}"
+echo 'start cleaning....'
 sudo make clean
 sudo rm -f "${script_path}/system/mkalteriso"
 
 cd - > /dev/null
+echo 'CLEAN DONE'
