092c5b8c0cdc34a78befdfeec50f7c0132cb0fc4
Tatsuto "Laminne" Yamamoto <laminne33569@approvers.dev>
2020-07-16 22:46:14 +0900

    名称のリプレース

--- channels/plasma/airootfs.any/root/customize_airootfs_plasma.sh
+++ channels/plasma/airootfs.any/root/customize_airootfs_plasma.sh
@@ -11,9 +11,9 @@ kernel='zen'
 theme_name=alter-logo
 rebuild=false
 japanese=false
-username='alter'
-os_name="Alter Linux"
-install_dir="alter"
+username='arch'
+os_name="Swilner Linux"
+install_dir="arch"
 usershell="/bin/bash"
 debug=true
 
