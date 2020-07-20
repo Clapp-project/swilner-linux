184fc338f903a154753e8369c3fbe22d57af31f3
Laminne Tatsuto Yamamoto <gtattoda4@gmail.com>
2020-07-21 01:30:00 +0900

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-set -e
+# set -e
 
-if [[ $UID != 0 ]]; then
-    echo "You have to run this as root." 1>&2
-    exit 1
-fi
+# if [[ $UID != 0 ]]; then
+#     echo "You have to run this as root." 1>&2
+#     exit 1
+# fi
 
-if ! type docker >/dev/null 2>&1; then
-    echo "You have to install docker." 1>&2
-    exit 1
-fi
+# if ! type docker >/dev/null 2>&1; then
+#     echo "You have to install docker." 1>&2
+#     exit 1
+# fi
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
