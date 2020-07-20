7ca5acb19af656f6a89daf38160667dbd5bab077
Laminne Tatsuto Yamamoto <gtattoda4@gmail.com>
2020-07-20 20:26:21 +0900

    巨大コミットの分割

--- channels/xfce-edition/.airootfs.x86_64/usr/share/lightdm-webkit/themes/visceral/cfg.js
+++
@@ -1,5 +0,0 @@
-var cfg_12h = true;
-var cfg_placeholder = "password";
-var cfg_bg_path = "assets/bg.jpg"
-var cfg_bg_blurred_path = "assets/bg-blurred.jpg"
-var cfg_font_family = "Ubuntu Regular, sans-serif"
